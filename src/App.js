2eb6ef81beceee8795cc4f22052ea0add35a62e4
Igor Mesquita <igormesquita@MacBook-Pro.local>
2020-06-27 15:34:11 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const apiKey = process.env.REACT_APP_API_KEY;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>My API KEY: {apiKey}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Documentação do React
         </a>
       </header>
     </div>
