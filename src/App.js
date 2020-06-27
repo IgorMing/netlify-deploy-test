76e480c8ea82cae57e5613c321e3ef32049f8686
Igor Mesquita <igormesquita@MacBook-Pro.local>
2020-06-27 15:41:00 -0300

    testing a simples change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Documentação do React
+          Documentação do React!!!
         </a>
       </header>
     </div>
